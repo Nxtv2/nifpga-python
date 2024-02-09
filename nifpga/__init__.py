a0141f6e41ac126f726c484acc6d9761d2c4d285
Ahmed Eshra <ahmed.eshra@ni.com>
2024-02-09 16:23:29 +0100

    Update __init__.py to add P2PStream & start_p2p_fifo

--- nifpga/__init__.py
+++ nifpga/__init__.py
@@ -3,5 +3,6 @@ from .statuscheckedlibrary import FunctionInfo, StatusCheckedLibrary
 from .nifpga import *
 from .session import Session
 from .bitfile import Bitfile
+from .niriop2p import P2PStream, start_p2p_fifo
 
 # flake8: noqa
